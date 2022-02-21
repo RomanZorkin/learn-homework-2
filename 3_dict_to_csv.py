3925c9e2d573e88a3876692f1e5a6a6ff676177e
astra <zorkin.roman@gmail.com>
2022-02-21 03:17:01 +0300

    Solved third exercise

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,32 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+from pathlib import Path
+
+target_file = Path('files/personal.csv')
+personal = [
+    {'name': 'Vasy', 'age': 21, 'job': 'Hairdresser'},
+    {'name': 'Pety', 'age': 68, 'job': 'Conductor'},
+    {'name': 'Nina', 'age': 19, 'job': 'Miner'},
+    {'name': 'Ivan', 'age': 45, 'job': 'Deputy'},
+    {'name': 'Galy', 'age': 19, 'job': 'Chief Executive'},
+    {'name': 'Juli', 'age': 32, 'job': 'Resuscitator', 'salary': 90000},
+]
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open(target_file, 'w', encoding='utf-8') as csv_file:
+        head = set().union(*(man.keys() for man in personal))
+        writer = csv.DictWriter(csv_file, head, delimiter=';')
+        writer.writeheader()
+        for man in personal:
+            writer.writerow(man)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
